8831c8b4ac45f9456154431dbb37dac71442ed6f
Estmaza <estmaza@MacBook-Pro-Estmaza.local>
2018-03-12 08:54:56 +0200

    init

---
+++ vue-cli-simple/cli-simple/src/Type.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <span>{{ message }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "type",
+  data() {
+    return {
+      text: "Hello from Type component"
+    };
+  }
+};
+</script>
+
+<style>
+
+</style>
